c5ab1c4017f7c13469191d8597e1fdb21aca7d67
Alexsandre Macaulay Machado da Silva <alex@MacBook-Air-de-Alexsandre.local>
2023-10-23 11:02:20 -0300

    Add new routes for session management

--- app.py
+++ app.py
@@ -227,5 +227,5 @@ def getapiv1(parameter):
         return "Erro! Não foi possível obter os dados. Tente novamente mais tarde."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
